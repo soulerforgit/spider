bfa88a72af6c4a3970275f34ab918764b697d8c3
xuewensi <xuewensi@yscredit.com>
2018-01-31 22:58:24 +0800

    project对象构思

--- LaSpider/lib/project.py
+++ LaSpider/lib/project.py
@@ -1,6 +1,20 @@
+project 检测result， 状态有页数和清洗结果
+主要用来处理统计页面。
+一个project应该包括爬虫和清洗，目前先考虑所有清洗在爬虫处理的情况
+
+class Field:
+    pass
+
+class StringField(Field):
+    """判断代码"""
+    raise Exception()
+    return super.__init__()
+
+
 class ProjectManager:
     def __init__(self, script, task):
         pass
+        self.page = page
 
     def build_module(self, script, task):
         pass
